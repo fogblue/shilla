4f47654a365328bf5c349cae8a9e4b1c8bb9da91
peeraal <leeh8687@gmail.com>
2018-10-05 16:44:46 +0900

    예약확인/취소 페이지 html 수정

--- src/main/java/iot5/project/shilla/IndexController.java
+++ src/main/java/iot5/project/shilla/IndexController.java
@@ -8,17 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	/*@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("대문입장");
 		
 		return "index";
+	}*/
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(Locale locale, Model model) {
+		logger.info("대문입장");
+		
+		return new ModelAndView("index");
 	}
 	
 }

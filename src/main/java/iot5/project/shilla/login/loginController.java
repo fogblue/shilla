a4b4b8a2e425543fef0ff9a624b4236b42211c91
parkhm333 <gjrdjrdjr@naver.com>
2018-10-05 17:11:35 +0900

    #8 멤버모델

--- src/main/java/iot5/project/shilla/login/loginController.java
+++ src/main/java/iot5/project/shilla/login/loginController.java
@@ -1,61 +1,118 @@
 package iot5.project.shilla.login;
+
+import java.io.IOException;
 import java.util.Locale;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
+import iot5.project.shilla.helper.MailHelper;
+import iot5.project.shilla.helper.RegexHelper;
+import iot5.project.shilla.helper.UploadHelper;
+import iot5.project.shilla.helper.Util;
+import iot5.project.shilla.helper.WebHelper;
+import iot5.project.shilla.model.Member;
 
 @Controller
 public class loginController {
-private static final Logger logger = LoggerFactory.getLogger(loginController.class);
-	
+	private static final Logger logger = LoggerFactory.getLogger(loginController.class);
+	@Autowired
+	WebHelper web;
+	@Autowired
+	RegexHelper regex;
+	@Autowired
+	UploadHelper upload;
+	@Autowired
+	MailHelper mail;
+	@Autowired
+	Util util;
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/login/log_main.do", method = RequestMethod.GET)
-	public ModelAndView log_main(Locale locale, Model model,HttpServletRequest request, 
-			HttpServletResponse response) {
+	public ModelAndView log_main(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response) {
 		logger.info("Welcome home! The client locale is {log_main}.", locale);
 		return new ModelAndView("login/log_main");
 	}
-	@RequestMapping(value = "/login/log_user",method = RequestMethod.GET)
-	public ModelAndView log_user(Locale locale, Model model,HttpServletRequest request, 
-			HttpServletResponse response) {
+
+	@RequestMapping(value = "/login/log_user", method = RequestMethod.GET)
+	public ModelAndView log_user(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response) {
 		logger.info("Welcome home! The client locale is {log_join01}.", locale);
 		return new ModelAndView("login/log_user");
 	}
-	@RequestMapping(value = "/login/log_unuser",method = RequestMethod.GET)
-	public ModelAndView log_unuser(Locale locale, Model model,HttpServletRequest request, 
+
+	@RequestMapping(value = "/login/log_unuser", method = RequestMethod.GET)
+	public ModelAndView log_unuser(Locale locale, Model model, HttpServletRequest request,
 			HttpServletResponse response) {
 		logger.info("Welcome home! The client locale is {log_join01}.", locale);
 		return new ModelAndView("login/log_unuser");
 	}
+
+	@RequestMapping(value = "/login/login_ok.do", method = RequestMethod.POST)
+	public ModelAndView LoginOk(Locale locale, Model model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		web.init();
+		
+		if(web.getSession("loginInfo")!=null) {
+			return web.redirect(web.getRootPath() + "/index.do", "이미 로그인 하셨습니다.");
+			
+		}
+		/**(4)파라미터 처리*/
+		String userId = web.getString("user_id");
+		String userPw = web.getString("user_pw");
+		
+		logger.debug("userId=" + userId);
+		logger.debug("userPw=" + userPw);
+		
+		if(userId == null || userPw == null) {
+			return web.redirect(null, "아이디나 비밀번호가 없습니다.");
+			
+		}
+		
+		/**(5)전달받은 파라미터를 Beans에 설정*/
+		Member member = new Member();
+		member.setUserId(userId);
+		member.setUserPw(userPw);
+		
+		
+		String movePage = request.getHeader("referer");
+		if(movePage == null) {
+			movePage = web.getRootPath() + "/index.do";
+		}
+		//sqlSession.close();
+		return web.redirect(movePage, null);
+	
+	}
+	
 	@RequestMapping(value = "/login/log_join01.do", method = RequestMethod.GET)
-	public ModelAndView log_join01(Locale locale, Model model,HttpServletRequest request, 
+	public ModelAndView log_join01(Locale locale, Model model, HttpServletRequest request,
 			HttpServletResponse response) {
 		logger.info("Welcome home! The client locale is {log_join01}.", locale);
 		return new ModelAndView("login/log_join01");
 	}
+
 	@RequestMapping(value = "/login/log_join02.do", method = RequestMethod.POST)
-	public ModelAndView log_join02(Locale locale, Model model,HttpServletRequest request, 
+	public ModelAndView log_join02(Locale locale, Model model, HttpServletRequest request,
 			HttpServletResponse response) {
 		logger.info("Welcome home! The client locale is {log_join02}.", locale);
 		return new ModelAndView("login/log_join02");
 	}
+
 	@RequestMapping(value = "/login/log_join03.do", method = RequestMethod.POST)
-	public ModelAndView log_join03(Locale locale, Model model,HttpServletRequest request, 
+	public ModelAndView log_join03(Locale locale, Model model, HttpServletRequest request,
 			HttpServletResponse response) {
 		logger.info("Welcome home! The client locale is {log_join03}.", locale);
 		return new ModelAndView("login/log_join03");
 	}
-	
+
 }
---
+++ src/main/java/iot5/project/shilla/model/Member.java
@@ -0,0 +1,111 @@
+package iot5.project.shilla.model;
+
+public class Member {
+	private int id;
+	private String userNameKor;
+	private String userNameEng;
+	private String userId;
+	private String userPw;
+	private String birthdate;
+	private String email;
+	private String tel;
+	private String telHome;
+	private String zipcode;
+	private String loadAddr;
+	private String detail;
+	private String regDate;
+	private String editDate;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserNameKor() {
+		return userNameKor;
+	}
+	public void setUserNameKor(String userNameKor) {
+		this.userNameKor = userNameKor;
+	}
+	public String getUserNameEng() {
+		return userNameEng;
+	}
+	public void setUserNameEng(String userNameEng) {
+		this.userNameEng = userNameEng;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getTelHome() {
+		return telHome;
+	}
+	public void setTelHome(String telHome) {
+		this.telHome = telHome;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getLoadAddr() {
+		return loadAddr;
+	}
+	public void setLoadAddr(String loadAddr) {
+		this.loadAddr = loadAddr;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getEditDate() {
+		return editDate;
+	}
+	public void setEditDate(String editDate) {
+		this.editDate = editDate;
+	}
+	@Override
+	public String toString() {
+		return "member [id=" + id + ", userNameKor=" + userNameKor + ", userNameEng=" + userNameEng + ", userId="
+				+ userId + ", userPw=" + userPw + ", birthdate=" + birthdate + ", email=" + email + ", tel=" + tel
+				+ ", telHome=" + telHome + ", zipcode=" + zipcode + ", loadAddr=" + loadAddr + ", detail=" + detail
+				+ ", regDate=" + regDate + ", editDate=" + editDate + "]";
+	}
+	
+	
+}

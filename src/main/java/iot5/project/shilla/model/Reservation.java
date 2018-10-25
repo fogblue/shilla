6c354cd17bffbf46f247043696ac433f3c7a1649
fogblue <fogblue.git@gmail.com>
2018-10-25 09:56:04 +0900

    DB 수정

---
+++ src/main/java/iot5/project/shilla/model/Reservation.java
@@ -0,0 +1,204 @@
+package iot5.project.shilla.model;
+
+public class Reservation {
+	private int roomId;
+	private int roomNo;
+	private String checkIn;
+	private String checkOut;
+	private String roomType;
+	private String packageType;
+	private int exbed;
+	private int meal;
+	private int totalPrice;
+	private String resvDate;
+	private int memberId;
+	private String hotelCate;
+	private int guestId;
+	private String cardNo;
+	private String cardType;
+	private int cardYy;
+	private int cardMm;
+	private int pplAd;
+	private int pplCh;
+	private int pplBb;
+	private String detail;
+
+	public int getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public String getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(String checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public String getCheckOut() {
+		return checkOut;
+	}
+
+	public void setCheckOut(String checkOut) {
+		this.checkOut = checkOut;
+	}
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+
+	public String getPackageType() {
+		return packageType;
+	}
+
+	public void setPackageType(String packageType) {
+		this.packageType = packageType;
+	}
+
+	public int getExbed() {
+		return exbed;
+	}
+
+	public void setExbed(int exbed) {
+		this.exbed = exbed;
+	}
+
+	public int getMeal() {
+		return meal;
+	}
+
+	public void setMeal(int meal) {
+		this.meal = meal;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getResvDate() {
+		return resvDate;
+	}
+
+	public void setResvDate(String resvDate) {
+		this.resvDate = resvDate;
+	}
+
+	public int getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getHotelCate() {
+		return hotelCate;
+	}
+
+	public void setHotelCate(String hotelCate) {
+		this.hotelCate = hotelCate;
+	}
+
+	public int getGuestId() {
+		return guestId;
+	}
+
+	public void setGuestId(int guestId) {
+		this.guestId = guestId;
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public String getCardType() {
+		return cardType;
+	}
+
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+
+	public int getCardYy() {
+		return cardYy;
+	}
+
+	public void setCardYy(int cardYy) {
+		this.cardYy = cardYy;
+	}
+
+	public int getCardMm() {
+		return cardMm;
+	}
+
+	public void setCardMm(int cardMm) {
+		this.cardMm = cardMm;
+	}
+
+	public int getPplAd() {
+		return pplAd;
+	}
+
+	public void setPplAd(int pplAd) {
+		this.pplAd = pplAd;
+	}
+
+	public int getPplCh() {
+		return pplCh;
+	}
+
+	public void setPplCh(int pplCh) {
+		this.pplCh = pplCh;
+	}
+
+	public int getPplBb() {
+		return pplBb;
+	}
+
+	public void setPplBb(int pplBb) {
+		this.pplBb = pplBb;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	@Override
+	public String toString() {
+		return "Reservation [roomId=" + roomId + ", roomNo=" + roomNo + ", checkIn=" + checkIn + ", checkOut="
+				+ checkOut + ", roomType=" + roomType + ", packageType=" + packageType + ", exbed=" + exbed + ", meal="
+				+ meal + ", totalPrice=" + totalPrice + ", resvDate=" + resvDate + ", memberId=" + memberId
+				+ ", hotelCate=" + hotelCate + ", guestId=" + guestId + ", cardNo=" + cardNo + ", cardType=" + cardType
+				+ ", cardYy=" + cardYy + ", cardMm=" + cardMm + ", pplAd=" + pplAd + ", pplCh=" + pplCh + ", pplBb="
+				+ pplBb + ", detail=" + detail + "]";
+	}
+
+}
--- src/main/java/iot5/project/shilla/model/ResvRoom.java
+++ src/main/java/iot5/project/shilla/model/ResvRoom.java
@@ -13,78 +13,103 @@ public class ResvRoom {
 	private String resvDate;
 	private int memberId;
 	private String hotelCate;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getRoomNo() {
 		return roomNo;
 	}
+
 	public void setRoomNo(int roomNo) {
 		this.roomNo = roomNo;
 	}
+
 	public String getCheckIn() {
 		return checkIn;
 	}
+
 	public void setCheckIn(String checkIn) {
 		this.checkIn = checkIn;
 	}
+
 	public String getCheckOut() {
 		return checkOut;
 	}
+
 	public void setCheckOut(String checkOut) {
 		this.checkOut = checkOut;
 	}
+
 	public String getRoomType() {
 		return roomType;
 	}
+
 	public void setRoomType(String roomType) {
 		this.roomType = roomType;
 	}
+
 	public String getPackageType() {
 		return packageType;
 	}
+
 	public void setPackageType(String packageType) {
 		this.packageType = packageType;
 	}
+
 	public int getExbed() {
 		return exbed;
 	}
+
 	public void setExbed(int exbed) {
 		this.exbed = exbed;
 	}
+
 	public int getMeal() {
 		return meal;
 	}
+
 	public void setMeal(int meal) {
 		this.meal = meal;
 	}
+
 	public int getTotalPrice() {
 		return totalPrice;
 	}
+
 	public void setTotalPrice(int totalPrice) {
 		this.totalPrice = totalPrice;
 	}
+
 	public String getResvDate() {
 		return resvDate;
 	}
+
 	public void setResvDate(String resvDate) {
 		this.resvDate = resvDate;
 	}
+
 	public int getMemberId() {
 		return memberId;
 	}
+
 	public void setMemberId(int memberId) {
 		this.memberId = memberId;
 	}
+
 	public String getHotelCate() {
 		return hotelCate;
 	}
+
 	public void setHotelCate(String hotelCate) {
 		this.hotelCate = hotelCate;
 	}
+
 	@Override
 	public String toString() {
 		return "ResvRoom [id=" + id + ", roomNo=" + roomNo + ", checkIn=" + checkIn + ", checkOut=" + checkOut
@@ -92,5 +117,5 @@ public class ResvRoom {
 				+ ", totalPrice=" + totalPrice + ", resvDate=" + resvDate + ", memberId=" + memberId + ", hotelCate="
 				+ hotelCate + "]";
 	}
-		
+
 }
